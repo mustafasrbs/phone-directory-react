6e0b7382062256e5e81b4d72cca9c942bc435b94
Mustafa <mustafasrbs1@gmail.com>
2024-12-19 19:57:59 +0300

    state and funtions added

--- src/components/Contatcs/Form/index.js
+++ src/components/Contatcs/Form/index.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Form() {
+const [form, setForm] = useState({fullname:"",phone_number:""})
+const onChangeInput = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+}
+const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname==="" || form.phone_number==="") {
+        return false;
+    }
+    console.log(form)
+}
+
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
         <div>
-            <input name='fullname' placeholder='Full Name' />
+            <input name='fullname' placeholder='Full Name' onChange={onChangeInput} />
         </div> 
         
         <div>
-            <input name='phone_number' placeholder='Phone Number' />
+            <input name='phone_number' placeholder='Phone Number' onChange={onChangeInput} />
         </div>
         <div>
             <button>Add</button>
         </div>
-    </div>
+    </form>
   )
 }
 
