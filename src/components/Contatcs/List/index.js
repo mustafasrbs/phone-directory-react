f42cb6a9a49511ba0ff6945476e3db2f51995938
Mustafa <mustafasrbs1@gmail.com>
2024-12-24 15:41:28 +0300

    array filtered

--- src/components/Contatcs/List/index.js
+++ src/components/Contatcs/List/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => {
+      return item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLowerCase());
+    });
+  });
   return (
     
     <div>
-      <input placeholder='Filter contact'></input>
+      <input 
+      placeholder='Filter contact'
+      value={filterText}
+      onChange={(e) => setFilterText(e.target.value)}
+      />
       <ul>
        {
-        contacts.map((contact, i)=> 
+        filtered.map((contact, i)=> 
           <li key={i}>
             {contact.fullname}
           </li>
